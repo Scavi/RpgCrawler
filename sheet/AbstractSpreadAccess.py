f5a5d0a622c89e03430f9b3f4861599d2d25464f
Michael Heymel <cfa.scavenger@gmx.de>
2018-03-20 21:41:10 +0100

    refactoring

--- sheet/AbstractSpreadAccess.py
+++ sheet/AbstractSpreadAccess.py
@@ -1,27 +1,20 @@
 import abc
+
 from requests.structures import CaseInsensitiveDict
 
 
 class AbstractSpreadAccess(metaclass=abc.ABCMeta):
+
     @abc.abstractmethod
-    def crawl_column(self, column_position: int) -> list:
-        """This method crawls the column in the main sheet of the crawler. The column defines the context / story
-        to be crawled (e.g. a treasure might contain gems, coins, ...).
+    def crawl_main_sheet(self) -> list:
+        """ This method crawls the column for tables in the main sheet of the crawler.
+        The column defines the context / story to be crawled (e.g. a treasure might contain gems, coins, ...).
         Every cell in the column contains a table that is part of the context.
 
-        :param column_position the position of the column in the core excel document
         :return a list with all table names in the column
         """
         pass
 
-    @abc.abstractmethod
-    def crawl_table(self, table_name: str) -> None:
-        """ This method crawls the table to the given table name, if the table is not known yet.
-        The table contains two columns (chance / text) and multiple rows
-
-        :param table_name the name of the table
-        """
-        pass
 
     @abc.abstractmethod
     def get_table(self, table_name: str):
@@ -33,14 +26,36 @@ class AbstractSpreadAccess(metaclass=abc.ABCMeta):
         """
         pass
 
+
     @abc.abstractmethod
     def table_access(self) -> CaseInsensitiveDict():
         """ :return all known table names to their tables"""
         pass
 
+
     @abc.abstractmethod
-    def story_context(self, column_position: int) -> str:
+    def story_context(self) -> str:
+        """ :return the context / name of the story """
+        pass
+
+
+    @abc.abstractmethod
+    def read_range(self) -> int:
         """
-        :param column_position the position of the column of the current story context
-        :return the name of the story """
+        Determines the range of rows to be read. This is required since the access on the spread sheet might
+        be very slow. Due to this, rows will be read in chunks
+
+        :return the range of rows to be read"""
+        pass
+
+
+    @abc.abstractmethod
+    def chance_range_column_pattern(self) -> str:
+        """:return the range pattern for the chance column in data tables """
+        pass
+
+
+    @abc.abstractmethod
+    def text_range_column_pattern(self) -> str:
+        """:return the range pattern for the text column in data tables """
         pass
