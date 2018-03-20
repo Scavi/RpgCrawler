9084202a7cef37816affc36ed22146ce9286debf
Michael Heymel <cfa.scavenger@gmx.de>
2018-03-20 21:35:12 +0100

    changed the way to format / print the result.

--- core/RpgCrawler.py
+++ core/RpgCrawler.py
@@ -5,18 +5,19 @@ from sheet.AbstractSpreadAccess import AbstractSpreadAccess
 class RpgCrawler:
 
     def __init__(self, spread_access: AbstractSpreadAccess, formatter: AbstractIO) -> None:
+        """ Constructor
+        :param spread_access the access to the spread sheet that contains all table to crawl
+        :param formatter the formatter for the output
+        """
         self.__spread_access = spread_access
         self.__formatter = formatter
 
-    def crawl(self, column_position: int):
+
+    def crawl(self):
         # all the tables of the story that needs to be iterated (e.g. gems, coins, armor of a treasure)
-        story_tables = self.__spread_access.crawl_column(column_position)
-
-        self.__formatter.print_story_context(self.__spread_access.story_context(column_position))
-
+        story_tables = self.__spread_access.crawl_main_sheet()
+        self.__formatter.print_story_context(self.__spread_access.story_context())
         for table_name in story_tables:
             # determines the current table (e.g. gems)
             table = self.__spread_access.table_access[table_name]
-            # determines the current row by chance
-            row = table.get_row_by_chance()
-            self.__formatter.print_story_line(row.generate())
+            self.__formatter.print_story_line(table)
