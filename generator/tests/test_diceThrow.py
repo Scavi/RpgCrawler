c8acddc8171f6478422e20d19d164e201eb3b754
Michael Heymel <cfa.scavenger@gmx.de>
2018-03-20 21:36:30 +0100

    initial version

---
+++ generator/tests/test_diceThrow.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+from generator import DiceThrow
+
+class TestDiceThrow(TestCase):
+    def test_process(self):
+        self.fail()
---
+++ generator/tests/test_tableReference.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestTableReference(TestCase):
+    def test_process(self):
+        self.fail()
